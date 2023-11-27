d05de60ba40f94e1c129c91f1bbc5916e4e145e9
Max <maxibbels55@gmail.com>
2023-11-27 12:43:10 +0000

    Update file AutoSklearnWrapper.py

--- AutoMLWrapper/automlwrapper/AutoSklearn/AutoSklearnWrapper.py
+++ AutoMLWrapper/automlwrapper/AutoSklearn/AutoSklearnWrapper.py
@@ -19,8 +19,8 @@ class AutoSklearnWrapper(AutoMLLibrary):
     #---------------------------------------------------------------------------------------------#
     def _data_preprocessing(self, data, target_column):
         
-        if self.custom__data_preprocessing_func is not None:
-            data = self.custom__data_preprocessing_func(data)
+        if self.custom_data_preprocessing_func is not None:
+            data = self.custom_data_preprocessing_func(data)
             return data
         
         X_train, y_train, X_test, y_test = self.split_and_separate(data, target_column, ratio=0.2, type='pandas')
