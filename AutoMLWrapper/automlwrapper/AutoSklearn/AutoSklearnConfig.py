790a6b504f14a6c03e9a9a3090765b5299c38cca
Max <maximilian.ibbels@stud.hn.de>
2024-02-28 13:03:55 +0000

    confused sklearn metric user mapping

--- AutoMLWrapper/automlwrapper/AutoSklearn/AutoSklearnConfig.py
+++ AutoMLWrapper/automlwrapper/AutoSklearn/AutoSklearnConfig.py
@@ -19,47 +19,47 @@ class AutoSklearnConfig(Configuration):
             params['memory_limit'] = 6000
             params['n_jobs'] = 5
         
-        metric = params.get('evaluation_metric', "")
+        metric = params.get('metric', "")
         if metric == 'accuracy':
-            params['evaluation_metric'] = autosklearn.metrics.accuracy
+            params['metric'] = autosklearn.metrics.accuracy
         elif metric == 'balanced_accuracy':
-            params['evaluation_metric'] = autosklearn.metrics.balanced_accuracy
+            params['metric'] = autosklearn.metrics.balanced_accuracy
         elif metric == 'f1':
-            params['evaluation_metric'] = autosklearn.metrics.f1
+            params['metric'] = autosklearn.metrics.f1
         elif metric == 'f1_macro':
-            params['evaluation_metric'] = autosklearn.metrics.f1_macro
+            params['metric'] = autosklearn.metrics.f1_macro
         elif metric == 'f1_micro':
-            params['evaluation_metric'] = autosklearn.metrics.f1_micro
+            params['metric'] = autosklearn.metrics.f1_micro
         elif metric == 'f1_weighted':
-            params['evaluation_metric'] = autosklearn.metrics.f1_weighted
+            params['metric'] = autosklearn.metrics.f1_weighted
         elif metric == 'roc_auc':
-            params['evaluation_metric'] = autosklearn.metrics.roc_auc
+            params['metric'] = autosklearn.metrics.roc_auc
         elif metric == 'precision':
-            params['evaluation_metric'] = autosklearn.metrics.precision
+            params['metric'] = autosklearn.metrics.precision
         elif metric == 'precision_macro':
-            params['evaluation_metric'] = autosklearn.metrics.precision_macro
+            params['metric'] = autosklearn.metrics.precision_macro
         elif metric == 'precision_weighted':
-            params['evaluation_metric'] = autosklearn.metrics.precision_weighted
+            params['metric'] = autosklearn.metrics.precision_weighted
         elif metric == 'average_precision':
-            params['evaluation_metric'] = autosklearn.metrics.average_precision
+            params['metric'] = autosklearn.metrics.average_precision
         elif metric == 'recall':
-            params['evaluation_metric'] = autosklearn.metrics.recall
+            params['metric'] = autosklearn.metrics.recall
         elif metric == 'recall_macro':
-            params['evaluation_metric'] = autosklearn.metrics.recall_macro
+            params['metric'] = autosklearn.metrics.recall_macro
         elif metric == 'recall_weighted':
-            params['evaluation_metric'] = autosklearn.metrics.recall_weighted
+            params['metric'] = autosklearn.metrics.recall_weighted
         elif metric == 'log_loss':
-            params['evaluation_metric'] = autosklearn.metrics.log_loss
+            params['metric'] = autosklearn.metrics.log_loss
         elif metric == 'r2':
-            params['evaluation_metric'] = autosklearn.metrics.r2
+            params['metric'] = autosklearn.metrics.r2
         elif metric == 'mean_squared_error':
-            params['evaluation_metric'] = autosklearn.metrics.mean_squared_error
+            params['metric'] = autosklearn.metrics.mean_squared_error
         elif metric == 'mean_absolute_error':
-            params['evaluation_metric'] = autosklearn.metrics.mean_absolute_error
+            params['metric'] = autosklearn.metrics.mean_absolute_error
         elif metric == 'median_absolute_error':
-            params['evaluation_metric'] = autosklearn.metrics.median_absolute_error
+            params['metric'] = autosklearn.metrics.median_absolute_error
         else:
-            params['evaluation_metric'] = autosklearn.metrics.accuracy
+            params['metric'] = autosklearn.metrics.accuracy
 
         return params
     #---------------------------------------------------------------------------------------------#
